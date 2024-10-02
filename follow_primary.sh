ece04e26778954cbae543774c8a1f245f19bb5f7
nathanalsan <53538745+nathanalsan@users.noreply.github.com>
2024-10-02 03:14:44 -0300

    Update follow_primary.sh

--- follow_primary.sh
+++ follow_primary.sh
@@ -94,7 +94,7 @@ ssh -T ${SSH_OPTIONS} ${POSTGRESQL_STARTUP_USER}@${NODE_HOST} "
     [ -d \"${NODE_PGDATA}\" ] && rm -rf ${NODE_PGDATA}/pg_replslot/*
 
     cat > ${RECOVERYCONF} << EOT
-primary_conninfo = 'host=${NEW_PRIMARY_NODE_HOST} port=${NEW_PRIMARY_NODE_PORT} user=${REPLUSER} application_name=${NODE_HOST} passfile=''${PGPASS_PATH}.pgpass'''
+primary_conninfo = 'host=${NEW_PRIMARY_NODE_HOST} port=${NEW_PRIMARY_NODE_PORT} user=${REPLUSER} application_name=${NODE_HOST} passfile=''${PGPASS_PATH}/.pgpass'''
 recovery_target_timeline = 'latest'
 primary_slot_name = '${REPL_SLOT_NAME}'
 EOT
